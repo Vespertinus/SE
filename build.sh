758906f5e14613f2a7d5470d559a4e25bff370d5
Vespertinus <rrvespertinus@gmail.com>
2017-08-27 16:57:28 +0300

    [offscreen_rendering] move files to sub dirs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "build simple_engine:"
-g++ -Wall -pedantic main.cpp InputManager.cpp -I./ -L/usr/local/lib  -o simple_engine -I/usr/include/OIS -I/usr/include/loki  -std=c++0x -L/usr/lib/x86_64-linux-gnu/ -lX11 -lXxf86vm -ggdb3 -lGL -lGLU -lOIS -O0 -lpthread
+g++ -Wall -pedantic ./samples/simple_engine/main.cpp core/InputManager.cpp -I./core -I./common -I./X11 -I./units -I./samples/simple_engine -L/usr/local/lib  -o simple_engine -I/usr/include/OIS -I/usr/include/loki  -std=c++0x -L/usr/lib/x86_64-linux-gnu/ -lX11 -lXxf86vm -ggdb3 -lGL -lGLU -lOIS -O0 -lpthread
 
 echo "build funny_tex:"
-g++ -Wall -pedantic FunnyTex.cpp -I./ -lGL -lGLU -lOIS -L/usr/local/lib  -o funny_tex -I/usr/include/OIS -I/usr/include/loki InputManager.cpp -std=c++0x -L/usr/X11R6/lib -lX11 -lXxf86vm -ggdb3 -O0 -lpthread
+g++ -Wall -pedantic ./samples/funny_tex/FunnyTex.cpp -I./core -I./common -I./X11 -I./units -I./samples/funny_tex -lGL -lGLU -lOIS -L/usr/local/lib  -o funny_tex -I/usr/include/OIS -I/usr/include/loki core/InputManager.cpp -std=c++0x -L/usr/X11R6/lib -lX11 -lXxf86vm -ggdb3 -O0 -lpthread
