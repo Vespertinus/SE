9ad10dad002d4e8cb53dacdc661e41d55831862a
Vespertinus <rrvespertinus@gmail.com>
2017-10-05 22:46:14 +0300

    [offscreen_rendering] cmake build on CentOS6

--- build_cpu.sh
+++ build_cpu.sh
@@ -1,5 +1,5 @@
 mkdir -p build_dir/
-CXX=g++-6 CC=gcc-6 cmake -H. -Bbuild_dir -DGPU=OFF -DBOOST_INCLUDEDIR:FILEPATH=/usr/include/boost154 -DBOOST_LIBRARYDIR:FILEPATH=/usr/lib64/boost154 -DGLIBCXX_USE_CXX11_ABI=0
+CXX=g++-6 CC=gcc-6 cmake -H. -Bbuild_dir -DGPU=OFF -DBOOST_INCLUDEDIR:FILEPATH=/usr/include/boost154 -DBOOST_LIBRARYDIR:FILEPATH=/usr/lib64/boost154 -DOLD_ABI=ON
 cd build_dir
 make -j `nproc`
 cd -
