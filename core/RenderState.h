3c51674ae24fa561367c22589112bc5b9abb3b83
Vespertinus <rrvespertinus@gmail.com>
2018-05-18 19:21:26 +0300

    [shader_attributes] basic OpenGL state managing

---
+++ core/RenderState.h
@@ -0,0 +1,51 @@
+
+#ifndef __RENDER_STATE_H__
+#define __RENDER_STATE_H__ 1
+
+#include <GLUtil.h>
+
+namespace SE {
+
+/*
+ basic stub for render state managing
+
+ all arguments for Set* must be alive until the end of frame
+ so it is not acceptable to store local variables
+ */
+class RenderState {
+
+        const glm::mat4 * pModelViewProjection;
+        const glm::mat4 * pTransformMat;
+        ShaderProgram   * pShader;
+
+        //TODO
+        //texture units
+        //variables
+
+        public:
+
+        RenderState();
+
+        template <class T> ret_code_t   SetVariable(const StrID name, const T & val);
+        ret_code_t                      SetTexture(const TextureUnit unit_index, const TTexture * pTex);
+        ret_code_t                      SetTexture(const StrID name, const TTexture * pTex);
+        void                            SetTransform(const glm::mat4 & oMat);
+        void                            SetViewProjection(const glm::mat4 & oMat);
+        void                            SetShaderProgram(ShaderProgram * pNewShader);
+        void                            Draw(const uint32_t vao_id,
+                                             const uint32_t triangles_cnt,
+                                             const uint32_t gl_index_type);
+        void                            Reset(); //TODO --> frame start
+
+};
+
+typedef Loki::SingletonHolder < RenderState >   TRenderState;
+
+} // namespace SE
+
+
+#ifdef SE_IMPL
+#include <RenderState.tcc>
+#endif
+
+#endif
