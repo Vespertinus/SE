79e8c96f92b44aaff2232b12452d2ade2a43fc8c
Vespertinus <rrvespertinus@gmail.com>
2019-07-31 18:59:47 +0300

    [cam_controller] lost app headers

---
+++ samples/offscreen_rendering/App.h
@@ -0,0 +1,39 @@
+
+#ifdef FORWARD_CUSTOM_COMPONENTS
+#ifndef FORWARD_CUSTOM_COMPONENTS_GUARD
+#define FORWARD_CUSTOM_COMPONENTS_GUARD
+
+namespace SE {
+
+#ifdef SE_INTERACTIVE        
+class BasicController;
+
+using TCustomComponents = MP::TypelistWrapper<BasicController>;
+#else
+
+using TCustomComponents = MP::TypelistWrapper<>;
+#endif
+
+}
+
+#endif
+#endif
+
+#if defined(INC_CUSTOM_COMPONENTS_HEADER) && defined (SE_INTERACTIVE)
+#ifndef INC_CUSTOM_COMPONENTS_HEADER_GUARD
+#define INC_CUSTOM_COMPONENTS_HEADER_GUARD
+
+#include <BasicController.h>
+
+#endif
+#endif
+
+#if defined(INC_CUSTOM_COMPONENTS_IMPL) && defined (SE_IMPL) && defined (SE_INTERACTIVE)
+#ifndef INC_CUSTOM_COMPONENTS_IMPL_GUARD
+#define INC_CUSTOM_COMPONENTS_IMPL_GUARD
+
+#include <BasicController.tcc>
+
+#endif
+#endif
+
